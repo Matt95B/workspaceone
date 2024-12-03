27f2d354120f877b3c197ffa05c62e7e512bec5a
Mathieu Beaugrand <beaugrandma@gmail.com>
2024-12-03 18:47:59 +1000

    upload file
    Signed-off-by: Mathieu Beaugrand

---
+++ scripts/Windows_Uninstall-Bloatwares.ps1
@@ -0,0 +1,43 @@
+# Remove bloatwares that are pre-installed in Windows
+# In Workspace ONE UEM console, create a script and paste the below code into the code field.
+
+# Name: Uninstall Bloatwares
+# Run: System context
+
+<#
+************************
+Preparation work to update the AppPatterns array
+Run the below commands to get a full list of Appx that are pre-installed in Windows and then decide which one you want to remove, paste the app name in the $AppPatterns array.
+
+$ListOfApps = Get-AppxPackage -AllUsers
+$ListOfApps | Select-Object -Property Name, PackageFullName
+************************
+#>
+
+# Retrieve all installed Appx packages for all users
+$ListOfApps = Get-AppxPackage -AllUsers
+
+# Define app patterns to match against. "*" could be used as wildcard characters.
+$AppPatterns = @(
+    "Microsoft.Xbox*",
+    "Microsoft.MicrosoftSolitaire*",
+    "Microsoft.Zune*",
+    "Microsoft.Skype*"
+)
+
+# Loop through patterns and remove matching packages
+foreach ($pattern in $AppPatterns) {
+    # Show which apps match the pattern
+    Write-Host "Looking for apps matching: $pattern"
+
+    $AppsToRemove = $ListOfApps | Where-Object { $_.PackageFullName -like $pattern }
+    
+    if ($AppsToRemove) {
+        foreach ($app in $AppsToRemove) {
+            Write-Host "Removing: $($app.PackageFullName)"
+            Remove-AppxPackage -Package $app.PackageFullName -ErrorAction SilentlyContinue
+        }
+    } else {
+        Write-Host "No apps found for pattern: $pattern"
+    }
+}
